a58269d0b169510f49997941db29b685d4f3e2df
Andrew castro <listerine1989@gmail.com>
2016-06-15 11:15:19 -0500

    latest

---
+++ controllers/company.js
@@ -0,0 +1,95 @@
+module.exports = function(app, apiRoutes, io){
+  	var _entity ="company";
+  	var _url_alias = "empresa";
+
+  	var path = require("path");
+    var mongoose = require('mongoose');
+    var Model = require(path.join("../", "models", _entity + ".js"));
+
+    function get(req, res){
+
+      var REQ = req.params; 
+
+       Model
+       .find()
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+
+
+    function getById(req, res){
+
+      var REQ = req.params; 
+
+       Model
+       .findOne({_id : REQ.id})
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+    function post(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+      !REQ.data || (data.data = REQ.data);
+      data._user = REQ._user; 
+      
+  	   var model = new Model(data);
+
+  		model.save(function(err, rs){
+    			if(rs){
+    				res.json(rs);
+    			}else{
+    				res.json(err);
+    			}
+  		});
+
+    }
+
+
+    function update(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+  		!REQ.data || (data.data = REQ.data);             
+
+  		data = { $set : data };          
+
+  		Model.update({ _id : mongoose.Types.ObjectId(req.params.id) }, data,function(err, rs){
+  			if(rs){
+  				res.json(err || rs);
+  			}
+  		});
+    }
+
+
+    function remove(req, res){
+        Model.remove({_id : mongoose.Types.ObjectId(req.params.id)}, function(err, rs){
+              if(!err)
+                  res.json(rs);
+              else
+                 res.status(500).json(err);
+        });
+	   }
+
+    apiRoutes.get("/" + _url_alias, get);
+    apiRoutes.get("/" + _url_alias + "/:id", getById);
+    apiRoutes.post("/" + _url_alias, post);
+    apiRoutes.put("/" + _url_alias + "/:id", update);
+    apiRoutes.delete("/" + _url_alias + "/:id", remove);
+
+    return this;
+}
