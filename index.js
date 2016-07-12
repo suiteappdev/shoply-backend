a2c674c4c8828e2af2dea368eb38d6ceff288a99
Andrew castro <listerine1989@gmail.com>
2016-07-11 19:50:06 -0500

    latest

--- index.js
+++ index.js
@@ -21,6 +21,34 @@ process.env.PWD = process.cwd() || process.env.PWD;
 
 apiRoutes = express.Router();
 
+apiRoutes.use(function(req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-shoply-auth'];
+        if (token) {
+            jwt.verify(token, app.get("secret"), function(err, decoded) {      
+                var Session = require("./models/session");
+                 if (err){
+                        return res.status(401).json({ success: false, message: 'Failed to authenticate token.' }); 
+                        Session.find({token : token}, function(err, rs){
+                            if(!err){ 
+                                    if(rs.length > 0){ 
+                                      req.decoded = decoded;    
+                                      next();
+                                   }
+                                   else{
+                                        res.status(401).json({ success : false, message : 'invalid token'});
+                                   }
+                            }
+                        })                 
+                 }
+          });
+        }else{
+          return res.status(403).send({ 
+              success: false, 
+              message: 'No token provided.' 
+          });
+        }
+    });
+
 app.use('/uploads', express.static('uploads'));
 
 app.get('/home', function(req, res){
