1e5e28cd3edab1d8992236e62f0c1a534c065f00
Andrew castro <listerine1989@gmail.com>
2016-07-13 18:28:48 -0500

    latest

--- controllers/apps.js
+++ controllers/apps.js
@@ -36,8 +36,8 @@ module.exports = function(app, apiRoutes, io){
 
               fs.readFile(path.join(process.env.PWD, "apps", "shoply-app", "config.xml"), function(err, data) {
                   parser.parseString(data, function (err, result) {
-                      result.widget.$.id = "com.ionicframework.shoplyapp722350";
-                      
+                      result.widget.$.id = "com.shoply." + res._id;
+
                       var xml2js = require('xml2js');
                       var builder = new xml2js.Builder();
                       var xml = builder.buildObject(result);
