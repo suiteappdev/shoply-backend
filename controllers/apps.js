1757dc648aea5dcf44f95efd74464ce4126dcd9b
Andrew castro <listerine1989@gmail.com>
2016-07-13 20:01:23 -0500

    latest

--- controllers/apps.js
+++ controllers/apps.js
@@ -36,6 +36,8 @@ module.exports = function(app, apiRoutes, io){
 
               fs.readFile(path.join(process.env.PWD, "apps", "shoply-app", "config.xml"), function(err, data) {
                   parser.parseString(data, function (err, result) {
+                      result.widget.$.id = "com.shoply." + rs._id;
+
                       var xml2js = require('xml2js');
                       var builder = new xml2js.Builder();
                       var xml = builder.buildObject(result);
@@ -66,6 +68,8 @@ module.exports = function(app, apiRoutes, io){
                       });                      
                   });
               });
+
+
           }
        });
     }
