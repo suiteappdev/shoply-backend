e6029e6c6583c579d4ae8f0efd17e217a6a21a06
Andrew castro <listerine1989@gmail.com>
2016-07-15 10:54:00 -0500

    latest

--- controllers/request.js
+++ controllers/request.js
@@ -91,7 +91,7 @@ module.exports = function(app, apiRoutes, io){
   		model.save(function(err, rs){
   			if(rs){
   				res.json(rs);
-          io.sockets.emit('request', rs);  			
+          io.to(rs._company).emit(rs):
         }else{
   				res.json(err);
   			}
