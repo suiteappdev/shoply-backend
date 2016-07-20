87fca5f61549fc5d89ef60718ce7de11a2bcee8e
Andrew castro <listerine1989@gmail.com>
2016-07-20 11:52:23 -0500

    latest

--- controllers/push.js
+++ controllers/push.js
@@ -23,6 +23,7 @@ module.exports = function(app, apiRoutes, io){
     function push(req, res){
 	    var device_tokens = []; //create array for storing device tokens
 	    var retry_times = 4; //the number of times to retry sending the message if it fails
+	    var REQ = req.body || req.params;
 
 	    var sender = new gcm.Sender('AIzaSyAS1CNxO5NB9ytZs-9iPS57fHFVLGTYhrg'); //create a new sender
 	    var message = new gcm.Message(); //create a new message
@@ -35,8 +36,8 @@ module.exports = function(app, apiRoutes, io){
 	    message.delayWhileIdle = true; //delay sending while receiving device is offline
 	    message.timeToLive = 3; //the number of seconds to keep the message on the server if the device is offline
 
-	   	User.findOne({ _id : mongoose.Types.ObjectId(REQ.user) }, function(err, rs){
-	    	device_tokens.push(rs.metadata.device);
+	   	User.findOne({ _id : mongoose.Types.ObjectId(REQ.user)}).exec(function(err, rs){
+	   		device_tokens.push(rs.metadata.device);
 	   	});
 
 	    sender.send(message, device_tokens, retry_times, function(result){
