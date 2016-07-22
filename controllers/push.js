393e67b18f63368e990931f4c700016c20a3b79e
Andrew castro <listerine1989@gmail.com>
2016-07-22 13:57:12 -0500

    latest

--- controllers/push.js
+++ controllers/push.js
@@ -13,10 +13,7 @@ module.exports = function(app, apiRoutes, io){
 
 		User.findOne({ _id : mongoose.Types.ObjectId(req.params.user)}, function(err, user){
 			if(!err){
-				user.data.device = req.params.device_token;
-				user.save(function(err){
-					res.status(200).json(user);
-				});
+				console.log(user);
 			}
 		}); 
     }
