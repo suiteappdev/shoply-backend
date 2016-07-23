ce8f510ce8d3c2013826cae162a932b9351fd47c
Andrew castro <listerine1989@gmail.com>
2016-07-23 11:36:20 -0500

    latest

---
+++ models/ivas.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Load required packages
+var timestamps = require('mongoose-timestamp');
+var metadata = require('./plugins/metadata');
+
+var entity = "ivas";
+
+var _Schema = new Schema({
+	  _company : {type : Schema.Types.ObjectId , ref : 'company'},
+	  data : { type : Object}
+ });
+
+_Schema.pre('save', function (next) {
+	next();
+});
+
+//add plugins
+_Schema.plugin(metadata);
+_Schema.plugin(timestamps);
+
+module.exports = mongoose.model(entity, _Schema); 
