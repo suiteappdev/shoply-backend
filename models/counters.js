6e200849b09bb45ac5ff026d679a75fae3df5d99
Andrew castro <listerine1989@gmail.com>
2016-07-24 15:13:11 -0500

    latest

--- models/counters.js
+++ models/counters.js
@@ -8,7 +8,7 @@ var metadata = require('./plugins/metadata');
 var entity = "counters";
 
 var _Schema = new Schema({
-	  entity : { type : String, trim : true, required: true}
+	  entity : { type : String, trim : true, required: true},
       seq : { type: Number},
 	  _company : { type : Schema.Types.ObjectId , ref : 'company'}
  });
