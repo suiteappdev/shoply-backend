73e20c9b37e12908f84874c3402d983f9341c523
Andrew castro <listerine1989@gmail.com>
2016-07-25 15:43:16 -0500

    latest

--- models/company.js
+++ models/company.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-function sq(collection, company, callback) {
-   mongoose.model('counters').findOneAndUpdate({ entity: collection, _company :  mongoose.Types.ObjectId(company)}, { $inc: { seq: 1 } }, callback);
-}
-
 // Load required packages
 var timestamps = require('mongoose-timestamp');
 var metadata = require('./plugins/metadata');
@@ -12,17 +8,12 @@ var metadata = require('./plugins/metadata');
 var entity = "company";
 
 var _Schema = new Schema({
-	   id : {type : Number, unique : true},
 	  _user : {type : Schema.Types.ObjectId , ref : 'User'},
 	  data : { type : Object}
  });
 
 _Schema.pre('save', function (next) {
-	_self = this;
-	sq("_company", _self._company, function(err, s){
-		_self.id = s.seq;
-		next();
-	});
+	next();
 });
 
 //add plugins
