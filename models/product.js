f05e3b1a7e905880a8156b0c7e5c64f0331210da
Andrew castro <listerine1989@gmail.com>
2016-07-27 14:10:04 -0500

    latest

--- models/product.js
+++ models/product.js
@@ -5,7 +5,12 @@ var path = require("path");
 
 // Load required packages
 
-function sq(collection, company, callback) {
+function sq(collection, company, rollback, callback) {
+   if(rollback){
+   		mongoose.model('counters').findOneAndUpdate({ entity: collection, _company :  mongoose.Types.ObjectId(company)}, { $inc: { seq: 1 } }, callback);
+   		return;
+   }	
+   
    mongoose.model('counters').findOneAndUpdate({ entity: collection, _company :  mongoose.Types.ObjectId(company)}, { $inc: { seq: 1 } }, callback);
 }
 
@@ -25,44 +30,44 @@ var _Schema = new Schema({
 
 _Schema.pre('save', function (next, done) {
 	_self = this;
-	 var _found = false;
-	 
-	 console.log(_self._id);
+	 var _found = [];
 
 	for(r in _self.data._reference){
 		var _reference = mongoose.model('reference');
 
-		_reference.findOne({reference : _self.data._reference[r], productId : _self.id}, function(err, ref){
+		_reference.findOne({reference : _self.data._reference[r]}, function(err, ref){
 			if(ref){
-				_found = true;
-  				_self.invalidate("duplicate", "duplicate reference");
-           		done({ code : 11000, reference: _self.data._reference[r]});
+				_found.push(_self.data._reference[r]);
 			}
 		});
 	}
 
-	if(!_found){
-		for(r in _self.data._reference){
-			var _ref = new _reference({
-				reference : _self.data._reference[r],
-				productId : _self.id,
-				_product : mongoose.Types.ObjectId(_self._id),
-				_company  :mongoose.Types.ObjectId(_self._company)
-			});			
-		}
 
-
-		_ref.save(function(err, rs){
-			console.log("reference", rs);
-			 //_self._reference[r] = mongoose.Types.ObjectId(rs._id);
-		});
-
-		/*console.log("no found");
+	if(_found.length == 0){
 		sq("_product", _self._company, function(err, s){
 			_self.id = s.seq;
-			console.log("saved")
-			next();
-		});	*/	
+			_self._reference = [];
+
+			for(r in _self.data._reference){
+				var _ref = new _reference({
+					reference : _self.data._reference[r],
+					productId : _self.id,
+					_product : mongoose.Types.ObjectId(_self._id),
+					_company  :mongoose.Types.ObjectId(_self._company)
+				});
+
+				_ref.save(function(err, rs){
+					if(rs){
+						_self._reference.push(mongoose.Types.ObjectId(rs._id)); 
+					}
+				})							
+			}
+
+			next();			
+		});	
+	}else{
+  		_self.invalidate("duplicate", "duplicate reference");
+        done({ code : 11000, reference: _found});
 	}
 });
 
