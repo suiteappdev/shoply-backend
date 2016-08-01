b3adbd0b8be118445285d68afc23fe2b4e77075d
Andrew castro <listerine1989@gmail.com>
2016-08-01 11:41:06 -0500

    lates

--- models/product.js
+++ models/product.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var entity = "product";
 var path = require("path");
-
 // Load required packages
 
 function sq(collection, company, callback) {
@@ -28,41 +27,32 @@ var _Schema = new Schema({
 
 _Schema.pre('save', function (next, done) {
 	_self = this;
+	var _reference = mongoose.model('reference');
+	
+	sq("_product", _self._company, function(err, s){
+		_self.id = s.seq;
 
-	for(r in _self.data._reference){
+		if(s.prefix){
+			_self.idcomposed = (s.prefix + s.seq);						
+		}
+
 		var _reference = mongoose.model('reference');
 
-		_reference.findOne({reference : _self.data._reference[r]}, function(err, ref){
-			if(ref){
-			  	_self.invalidate("duplicate", "duplicate reference");
-    			done({ code : 11000, reference:  _self.data._reference[r]});
-			}else{
-				sq("_product", _self._company, function(err, s){
-					_self.id = s.seq;
-
-					if(s.prefix){
-						_self.idcomposed = (s.prefix + s.seq);						
-					}
-
-					var _reference = mongoose.model('reference');
-
-					var _ref = new _reference({
-						reference : _self.data._reference || [_self.id.toString()],
-						productId : _self.id,
-						_product : mongoose.Types.ObjectId(_self._id),
-						_company  :mongoose.Types.ObjectId(_self._company)
-					});
-
-					_ref.save(function(err, rs){
-						if(rs){
-							_self._reference = mongoose.Types.ObjectId(rs._id);
-							delete _self.data._reference;
-							next();
-						}
-					})
-				});				
-			}
+		var _ref = new _reference({
+			reference : _self.data._reference || [_self.id.toString()],
+			productId : _self.id,
+			_product : mongoose.Types.ObjectId(_self._id),
+			_company  :mongoose.Types.ObjectId(_self._company)
 		});
+
+		_ref.save(function(err, rs){
+			if(rs){
+				_self._reference = mongoose.Types.ObjectId(rs._id);
+				delete _self.data._reference;
+				next();
+			}
+		})
+	});	
 	}
 });
 
