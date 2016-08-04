4c5558d9fa473980366d283e34eecc2683532c04
Andrew castro <listerine1989@gmail.com>
2016-08-04 11:49:54 -0500

    latest

---
+++ models/billing.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Load required packages
+var timestamps = require('mongoose-timestamp');
+var metadata = require('./plugins/metadata');
+
+var entity = "billing";
+
+var _Schema = new Schema({
+	_client :  { type : Object},
+	_product :  { type : Array},
+	_payments : { type : Array},
+	data : 		{ type : Object}
+ });
+
+_Schema.pre('save', function (next) {
+	next();
+});
+
+//add plugins
+_Schema.plugin(metadata);
+_Schema.plugin(timestamps);
+
+module.exports = mongoose.model(entity, _Schema); 
