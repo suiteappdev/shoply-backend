7717a85e82a0db028f64500df25cdf5067051a21
Andrew castro <listerine1989@gmail.com>
2016-08-20 11:54:26 -0500

    latest

--- controllers/billing.js
+++ controllers/billing.js
@@ -84,6 +84,7 @@ module.exports = function(app, apiRoutes, io){
       var REQ = req.body || req.params;
       var _data = {};
           _data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
+          _data.createdAt  = {};
 
       if(REQ.ini){
         _data.createdAt.$gte = new Date(REQ.ini).toISOString(); 
