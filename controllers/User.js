b97aca79663a57b69ba11883d7bb0497f19079f6
Andrew castro <listerine1989@gmail.com>
2016-09-13 13:00:54 -0500

    latest

--- controllers/User.js
+++ controllers/User.js
@@ -130,8 +130,7 @@ module.exports = function(app, apiRoutes){
     }
 
     function verificationCode(req, res){
-      
-      User.findOne({_id : mongoose.Types.ObjectId(req.params('user'))}).exec(function(err, user){
+      User.findOne({_id : mongoose.Types.ObjectId(req.params.user)}).exec(function(err, user){
           if(!err){
               crypto.pseudoRandomBytes(4, function (err, raw){
                   user.verificationCode =  raw.toString('hex');
