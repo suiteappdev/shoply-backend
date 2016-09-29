463064182bf8137cea6dd19e33d44147749b67e5
Andrew castro <listerine1989@gmail.com>
2016-09-29 15:27:44 -0500

    latest

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	dburl : "mongodb://armitage1989:house1989*@localhost:27017/shoply",
+	dburl : "mongodb://Armitage1989:House1989*@localhost:27017/shoply",
 	secret : "$shoply.2016",
 	appPort : 8080,
 	smtp_email : "listerine1989@gmail.com",
