15adf27cb4dc706ac35bfd3e6ad7691ebe5b8a22
Andrew castro <listerine1989@gmail.com>
2016-11-18 09:59:02 -0500

    latest

--- controllers/category.js
+++ controllers/category.js
@@ -76,7 +76,7 @@ module.exports = function(app, apiRoutes, io){
 
       !REQ.data || (data.data = REQ.data); 
       
-      data.id = mongoose.Types.ObjectId(REQ.id);      
+      data.id = new mongoose.Types.ObjectId();      
       data.text = REQ.text;
       data.parent = REQ.parent;
       data._company = req.headers["x-shoply-company"];
