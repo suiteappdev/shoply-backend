9320abb4ff712c38d1366b212d155ea4169808a7
Andrew castro <listerine1989@gmail.com>
2016-11-29 10:51:05 -0500

    latest

--- controllers/grocery.js
+++ controllers/grocery.js
@@ -1,105 +1,33 @@
 module.exports = function(app, apiRoutes, io){
-  	var _entity ="grocery";
-  	var _url_alias = "bodegas";
+  	var _entity ="amounts";
+  	var _url_alias = "cantidades";
 
   	var path = require("path");
     var mongoose = require('mongoose');
     var Model = require(path.join("../", "models", _entity + ".js"));
 
-    function get(req, res){
+    function add(req, res){
+    		var data = {};
+    		var REQ = req.body || req.params;
 
-      var REQ = req.params; 
-       Model
-       .find({_company : mongoose.Types.ObjectId(req.headers["x-shoply-company"])})
-       .populate("_company _responsible")
-       .exec(function(err, rs){
-           if(!err)
-           {
-            res.json(rs);
-           }
-           else
-            res.json(err);
-       });
-    }
-
-
-
-    function getById(req, res){
-
-      var REQ = req.params; 
-
-       Model
-       .findOne({_id : REQ.id})
-       .populate("_company _responsible")
-       .exec(function(err, rs){
-           if(!err)
-           {
-            res.json(rs);
-           }
-           else
-            res.json(err);
-       });
-    }
-
-    function post(req, res){
-  		var data = {};
-  		var REQ = req.body || req.params;
-
-      !REQ.data || (data.data = REQ.data);
-      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
-
-      if(REQ._responsible){
-         data._responsible = mongoose.Types.ObjectId(REQ._responsible);
-      }
-
-      var model = new Model(data);
-
-  		model.save(function(err, rs){
+    		Model.update({ _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"]), mongoose.Types.ObjectId(req.params.id) }, {$inc :{amount : req.params.amount}}, function(err, rs){
     			if(rs){
-    				res.json(rs);
-    			}else{
-    				res.json(err);
+    				  res.status(200).json(err || rs);
     			}
-  		});
-
-    }
-
-
-    function update(req, res){
-  		var data = {};
-  		var REQ = req.body || req.params;
-
-  		!REQ.data || (data.data = REQ.data); 
-      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
-
-      if(REQ._responsible){
-         data._responsible = mongoose.Types.ObjectId(REQ._responsible);
-      }
-
-  		data = { $set : data };          
-
-  		Model.update({ _id : mongoose.Types.ObjectId(req.params.id) }, data,function(err, rs){
-  			if(rs){
-  				res.json(err || rs);
-  			}
-  		});
+    		});
     }
 
 
     function remove(req, res){
-        Model.remove({_id : mongoose.Types.ObjectId(req.params.id)}, function(err, rs){
-              if(!err)
-                  res.json(rs);
-              else
-                 res.status(500).json(err);
+        Model.update({ _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"]), _product : mongoose.Types.ObjectId(req.params.id) }, {$dec :{amount : req.params.amount}}, function(err, rs){
+          if(rs){
+              res.status(200).json(err || rs);
+          }
         });
 	   }
 
-    apiRoutes.get("/" + _url_alias, get);
-    apiRoutes.get("/" + _url_alias + "/:id", getById);
-    apiRoutes.post("/" + _url_alias, post);
-    apiRoutes.put("/" + _url_alias + "/:id", update);
-    apiRoutes.delete("/" + _url_alias + "/:id", remove);
+    apiRoutes.get("/" + _url_alias + "/:id/add/:amount", add);
+    apiRoutes.get("/" + _url_alias + "/:id/remove/:amount", remove);
 
     return this;
 }
---
+++ models/amounts.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// Load required packages
+var timestamps = require('mongoose-timestamp');
+var metadata = require('./plugins/metadata');
+
+var entity = "amounts";
+
+var _Schema = new Schema({
+	  _company : {type : Schema.Types.ObjectId , ref : 'company'},
+	  _grocery : {type : Schema.Types.ObjectId , ref : 'grocery'},
+	  _product : {type : Schema.Types.ObjectId , ref : 'product'},
+	  amount : { type : Number}
+ });
+
+_Schema.pre('save', function (next) {
+	next();
+});
+
+//add plugins
+_Schema.plugin(metadata);
+_Schema.plugin(timestamps);
+
+module.exports = mongoose.model(entity, _Schema); 
