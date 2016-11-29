8ca5fe36068c4ccc699244e9221a00d519c6165e
Andrew castro <listerine1989@gmail.com>
2016-11-29 11:48:09 -0500

    latest

---
+++ controllers/amount.js
@@ -0,0 +1,33 @@
+module.exports = function(app, apiRoutes, io){
+  	var _entity ="amounts";
+  	var _url_alias = "cantidades";
+
+  	var path = require("path");
+    var mongoose = require('mongoose');
+    var Model = require(path.join("../", "models", _entity + ".js"));
+
+    function add(req, res){
+    		var data = {};
+    		var REQ = req.body || req.params;
+
+    		Model.update({ _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"]), _product : mongoose.Types.ObjectId(req.params.id)}, {$inc :{amount : req.params.amount}}, function(err, rs){
+    			if(rs){
+    				  res.status(200).json(err || rs);
+    			}
+    		});
+    }
+
+
+    function remove(req, res){
+        Model.update({ _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"]), _product : mongoose.Types.ObjectId(req.params.id) }, {$dec :{amount : req.params.amount}}, function(err, rs){
+          if(rs){
+              res.status(200).json(err || rs);
+          }
+        });
+	   }
+
+    apiRoutes.get("/" + _url_alias + "/:id/add/:amount", add);
+    apiRoutes.get("/" + _url_alias + "/:id/remove/:amount", remove);
+
+    return this;
+}
--- controllers/grocery.js
+++ controllers/grocery.js
@@ -1,33 +1,105 @@
 module.exports = function(app, apiRoutes, io){
-  	var _entity ="amounts";
-  	var _url_alias = "cantidades";
+  	var _entity ="grocery";
+  	var _url_alias = "bodegas";
 
   	var path = require("path");
     var mongoose = require('mongoose');
     var Model = require(path.join("../", "models", _entity + ".js"));
 
-    function add(req, res){
-    		var data = {};
-    		var REQ = req.body || req.params;
+    function get(req, res){
 
-    		Model.update({ _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"]), _product : mongoose.Types.ObjectId(req.params.id)}, {$inc :{amount : req.params.amount}}, function(err, rs){
+      var REQ = req.params; 
+       Model
+       .find({_company : mongoose.Types.ObjectId(req.headers["x-shoply-company"])})
+       .populate("_company _responsible")
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+
+
+    function getById(req, res){
+
+      var REQ = req.params; 
+
+       Model
+       .findOne({_id : REQ.id})
+       .populate("_company _responsible")
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+    function post(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+      !REQ.data || (data.data = REQ.data);
+      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
+
+      if(REQ._responsible){
+         data._responsible = mongoose.Types.ObjectId(REQ._responsible);
+      }
+
+      var model = new Model(data);
+
+  		model.save(function(err, rs){
     			if(rs){
-    				  res.status(200).json(err || rs);
+    				res.json(rs);
+    			}else{
+    				res.json(err);
     			}
-    		});
+  		});
+
+    }
+
+
+    function update(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+  		!REQ.data || (data.data = REQ.data); 
+      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
+
+      if(REQ._responsible){
+         data._responsible = mongoose.Types.ObjectId(REQ._responsible);
+      }
+
+  		data = { $set : data };          
+
+  		Model.update({ _id : mongoose.Types.ObjectId(req.params.id) }, data,function(err, rs){
+  			if(rs){
+  				res.json(err || rs);
+  			}
+  		});
     }
 
 
     function remove(req, res){
-        Model.update({ _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"]), _product : mongoose.Types.ObjectId(req.params.id) }, {$dec :{amount : req.params.amount}}, function(err, rs){
-          if(rs){
-              res.status(200).json(err || rs);
-          }
+        Model.remove({_id : mongoose.Types.ObjectId(req.params.id)}, function(err, rs){
+              if(!err)
+                  res.json(rs);
+              else
+                 res.status(500).json(err);
         });
 	   }
 
-    apiRoutes.get("/" + _url_alias + "/:id/add/:amount", add);
-    apiRoutes.get("/" + _url_alias + "/:id/remove/:amount", remove);
+    apiRoutes.get("/" + _url_alias, get);
+    apiRoutes.get("/" + _url_alias + "/:id", getById);
+    apiRoutes.post("/" + _url_alias, post);
+    apiRoutes.put("/" + _url_alias + "/:id", update);
+    apiRoutes.delete("/" + _url_alias + "/:id", remove);
 
     return this;
 }
