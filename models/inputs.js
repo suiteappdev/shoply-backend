9066adff5ab71f9042114eee6bc5cd3c7342f860
Andrew castro <listerine1989@gmail.com>
2016-12-01 14:03:41 -0500

    latest

--- models/inputs.js
+++ models/inputs.js
@@ -49,6 +49,8 @@ _Schema.post('save', function () {
 			_company: mongoose.Types.ObjectId(_self._company)
 		};
 
+		console.log("iteracion del _product" , _self.data._product[x].id);
+
 	    _amounts.findOne(where, function(err, rs){
 	        if(rs){
 	        	var amount = (rs.amount + parseInt(_self.data._product[x].cantidad));
@@ -66,6 +68,9 @@ _Schema.post('save', function () {
 	            	_product : mongoose.Types.ObjectId(_self.data._product[x]._id),
 	            	amount : parseInt(_self.data._product[x].cantidad)
 	            });
+
+				console.log("iteracion del _product save" , _self.data._product[x].id);
+
 	            
 	            inputs.save(function(err, rs){
 	            	if(!err){
