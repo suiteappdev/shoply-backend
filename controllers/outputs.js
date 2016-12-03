d371f9ebbc29e3439b33766830670f8e190c921d
Andrew castro <listerine1989@gmail.com>
2016-12-03 09:49:20 -0500

    latest

---
+++ controllers/outputs.js
@@ -0,0 +1,121 @@
+module.exports = function(app, apiRoutes, io){
+  	var _entity ="outputs";
+  	var _url_alias = "salidas";
+
+  	var path = require("path");
+    var mongoose = require('mongoose');
+    var Model = require(path.join("../", "models", _entity + ".js"));
+
+    function get(req, res){
+
+      var REQ = req.params; 
+       Model
+       .find({_company : mongoose.Types.ObjectId(req.headers["x-shoply-company"])})
+       .populate("_company _responsible _grocery")
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+
+
+    function getById(req, res){
+
+      var REQ = req.params; 
+
+       Model
+       .findOne({_id : REQ.id})
+       .populate("_company _responsible _grocery")
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+    function post(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+      !REQ.data || (data.data = REQ.data);
+      !REQ.metadata || (data.metadata = REQ.metadata);
+
+      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
+
+      if(REQ._responsible){
+         data._responsible = mongoose.Types.ObjectId(REQ._responsible);
+      }
+
+      if(REQ._author){
+         data._author = mongoose.Types.ObjectId(REQ._author);
+      }
+
+      if(REQ._grocery){
+         data._grocery = mongoose.Types.ObjectId(REQ._grocery);
+      }
+
+      var model = new Model(data);
+
+  		model.save(function(err, rs){
+    			if(rs){
+    				res.json(rs);
+    			}else{
+    				res.json(err);
+    			}
+  		});
+
+    }
+
+
+    function update(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+  		!REQ.data || (data.data = REQ.data);
+      !REQ.metadata || (data.metadata = REQ.metadata);
+
+      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
+
+      if(REQ._responsible){
+         data._responsible = mongoose.Types.ObjectId(REQ._responsible);
+      }
+
+      if(REQ._author){
+         data._author = mongoose.Types.ObjectId(REQ._author);
+      }
+
+  		data = { $set : data };          
+
+  		Model.update({ _id : mongoose.Types.ObjectId(req.params.id) }, data,function(err, rs){
+  			if(rs){
+  				res.json(err || rs);
+  			}
+  		});
+    }
+
+
+    function remove(req, res){
+        Model.remove({_id : mongoose.Types.ObjectId(req.params.id)}, function(err, rs){
+              if(!err)
+                  res.json(rs);
+              else
+                 res.status(500).json(err);
+        });
+	   }
+
+    apiRoutes.get("/" + _url_alias, get);
+    apiRoutes.get("/" + _url_alias + "/:id", getById);
+    apiRoutes.post("/" + _url_alias, post);
+    apiRoutes.put("/" + _url_alias + "/:id", update);
+    apiRoutes.delete("/" + _url_alias + "/:id", remove);
+
+    return this;
+}
---
+++ models/outputs.js
@@ -0,0 +1,80 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Q = require("q");
+
+function sq(collection, company, callback) {
+   mongoose.model('counters').findOneAndUpdate({ entity: collection, _company :  mongoose.Types.ObjectId(company)}, { $inc: { seq: 1 } }, callback);
+}
+
+// Load required packages
+var timestamps = require('mongoose-timestamp');
+var metadata = require('./plugins/metadata');
+
+var entity = "outputs";
+
+var _Schema = new Schema({
+	  id : {type : Number},
+	  idcomposed : { type : String},
+	  _company : {type : Schema.Types.ObjectId , ref : 'company'},
+	  _responsible : {type : Schema.Types.ObjectId , ref : 'User'},
+	  _grocery :{type : Schema.Types.ObjectId , ref : 'grocery'},
+	  _author : {type : Schema.Types.ObjectId , ref : 'User'},
+	  data : Object
+ });
+
+_Schema.pre('save', function (next) {
+	_self = this;
+	sq("_inputs", _self._company, function(err, s){
+		if(s){
+		
+			if(s.prefix){
+				_self.idcomposed = (s.prefix + s.seq);						
+			}
+
+			_self.id = s.seq;
+			next();			
+		}else{
+			next();
+		}
+	});
+});
+
+_Schema.post('save', function () {
+	var _self = this;
+	var _amounts = mongoose.model('amounts');
+	var _task = [];
+
+	for(x in _self.data._product){
+
+		var where = {
+			_grocery: mongoose.Types.ObjectId(_self._grocery),
+			_product : mongoose.Types.ObjectId(_self.data._product[x]._id),
+			_company: mongoose.Types.ObjectId(_self._company)
+		};
+
+		var data = {
+			_grocery: mongoose.Types.ObjectId(_self._grocery),
+			_product : mongoose.Types.ObjectId(_self.data._product[x]._id),
+			_company: mongoose.Types.ObjectId(_self._company),
+			$dec : {amount : _self.data._product[x].cantidad}
+		};
+
+		_task.push(_amounts.update(
+		   where,
+		   data,
+		   {
+		     upsert: true,
+		   }
+		).exec());
+	}
+
+	Q.all(_task).then(function(values){
+		console.log("result", values);
+	});
+});
+
+//add plugins
+_Schema.plugin(metadata);
+_Schema.plugin(timestamps);
+
+module.exports = mongoose.model(entity, _Schema); 
