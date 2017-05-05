11677c36eeb9012041f2ab9f6d4d7c5ec7d3008e
Carlos Vasquez <carlosvasquez2014@gmail.com>
2017-05-05 14:39:57 -0500

    lates

---
+++ controllers/lotes.js
@@ -0,0 +1,96 @@
+module.exports = function(app, apiRoutes, io){
+  	var _entity ="lotes";
+  	var _url_alias = "lotes";
+
+  	var path = require("path");
+    var mongoose = require('mongoose');
+    var Model = require(path.join("../", "models", _entity + ".js"));
+
+    function get(req, res){
+
+      var REQ = req.params; 
+
+       Model
+       .find({_company : mongoose.Types.ObjectId(req.headers["x-shoply-company"])})
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+    function getById(req, res){
+
+      var REQ = req.params; 
+
+       Model
+       .findOne({_id : REQ.id, _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"])})
+       .exec(function(err, rs){
+           if(!err)
+           {
+            res.json(rs);
+           }
+           else
+            res.json(err);
+       });
+    }
+
+    function post(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+      !REQ.data || (data.data = REQ.data);
+      !REQ.metadata || (data.metadata = REQ.metadata);
+      data._company = mongoose.Types.ObjectId(req.headers["x-shoply-company"]);
+
+  	   var model = new Model(data);
+
+  		model.save(function(err, rs){
+    			if(rs){
+    				res.json(rs);
+    			}else{
+    				res.json(err);
+    			}
+  		});
+
+    }
+
+
+    function update(req, res){
+  		var data = {};
+  		var REQ = req.body || req.params;
+
+  		!REQ.data || (data.data = REQ.data); 
+      !REQ.metadata || (data.metadata = REQ.metadata);
+                  
+
+  		data = { $set : data };          
+
+  		Model.update({ _id : mongoose.Types.ObjectId(req.params.id) }, data,function(err, rs){
+  			if(rs){
+  				res.json(err || rs);
+  			}
+  		});
+    }
+
+
+    function remove(req, res){
+        Model.remove({_id : mongoose.Types.ObjectId(req.params.id), _company : mongoose.Types.ObjectId(req.headers["x-shoply-company"])}, function(err, rs){
+              if(!err)
+                  res.json(rs);
+              else
+                 res.status(500).json(err);
+        });
+	   }
+
+    apiRoutes.get("/" + _url_alias, get);
+    apiRoutes.get("/" + _url_alias + "/:id", getById);
+    apiRoutes.post("/" + _url_alias, post);
+    apiRoutes.put("/" + _url_alias + "/:id", update);
+    apiRoutes.delete("/" + _url_alias + "/:id", remove);
+
+    return this;
+}
--- models/lotes.js
+++ models/lotes.js
@@ -1,27 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 // Load required packages
 var timestamps = require('mongoose-timestamp');
 var metadata = require('./plugins/metadata');
 
-var entity = "_productions";
+var entity = "lotes";
 
 var _Schema = new Schema({
+	  _company : {type : Schema.Types.ObjectId , ref : 'company'},
 	  data : { type : Object}
  });
 
 _Schema.pre('save', function (next) {
-	_self = this;
-	sq("_productions", _self._company, function(err, s){
-		if(s){
-			_self.id = s.seq;
-			next();			
-		}else{
-			next();
-		}
-	});
+	next();
 });
 
 //add plugins
